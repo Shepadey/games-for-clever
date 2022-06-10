87a8589ad0b2e784c2aeb97005f97fe02c778b6d
Иван Иванов <ivanivanov@MacBook-Pro-Ivan.local>
2022-06-10 16:11:30 +0300

    chenge password

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('login/',auth_views.LoginView.as_view(),name='login'),
     path('logout/',auth_views.LogoutView.as_view(),name='logout'),
     path('signup/',UserCreationView.as_view(),name='sign_up'),
+    path('password_change/',auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change_done/', auth_views.PasswordChangeDoneView.as_view(),name='password_change_done'),
 ]
 
