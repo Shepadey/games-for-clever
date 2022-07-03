b19a468a515a57bed2d71bc57bb3275f69987b18
Shepadey <dolechagy@gmail.com>
2022-07-03 12:24:26 +0300

    complete change password

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,5 +10,8 @@ urlpatterns = [
     path('password_reset/',auth_views.PasswordResetView.as_view(),name='password_reset'),
     path('password_reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
     path('password_reset_complete/',auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+
 ]
+
 
